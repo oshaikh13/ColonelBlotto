588e4bdf65fec5e1df3fff088ec29230feddf6eb
Omar Shaikh <oshaikh13@gmail.com>
2016-02-18 22:32:05 +0300

    Added socketHandler, cors, to base server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+var express     = require('express'),
+    mongoose    = require('mongoose');
+
+var app = express(),
+    cors = require('cors');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.use(cors());
+
+require('./config/socketHandler.js')(io); 
+
+// export our app for testing and flexibility, required by index.js
+
+server.listen(process.env.PORT || 8000);
+
+module.exports = app;
