de24ef6d3898fd795d00d2e2e6e438c1cfd9f251
Omar Shaikh <oshaikh13@gmail.com>
2016-02-22 07:17:01 +0300

    Added validator for createUser

--- server/user/userController.js
+++ server/user/userController.js
@@ -1,10 +1,24 @@
 var con = require('../db/connection.js');
 
 module.exports = {
+  valid: function(obj) {
+
+    if (Object.keys(obj).length === 2) {
+      if (obj.username && obj.password) {
+        return true;
+      }
+    }
+
+    return false;
+
+  },
+
   createUser: function (data, cb) {
-    if ()
-    con.query('INSERT into users SET ?', data, function(err, res) {
-      cb(err, res)
-    });
+    if (this.valid(data))  {    
+      con.query('INSERT into users SET ?', data, function(err, res) {
+        cb(err, res);
+      });
+    }
   }
 }
+
