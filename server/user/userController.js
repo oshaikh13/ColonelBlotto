9d8840cf87e5cd8fde6e88a05258bfbb34c64025
Omar Shaikh <oshaikh13@gmail.com>
2016-02-22 22:23:50 +0300

    Check if user exits

--- server/user/userController.js
+++ server/user/userController.js
@@ -1,6 +1,7 @@
 var con = require('../db/connection.js');
 
 module.exports = {
+
   valid: function(obj) {
 
     if (Object.keys(obj).length === 2) {
@@ -13,12 +14,26 @@ module.exports = {
 
   },
 
+  userExists: function(data, cb) {
+    con.query("SELECT * FROM users WHERE username = ? LIMIT 1", data.username, function(err, res) {
+      cb(err, res)
+    })
+  },
+
   createUser: function (data, cb) {
-    if (this.valid(data))  {    
-      con.query('INSERT into users SET ?', data, function(err, res) {
-        cb(err, res);
-      });
+    if (this.valid(data))  {  
+      this.userExists(data, function(err, res){
+        if (!res.length) {
+          con.query('INSERT into users SET ?', data, function(err, res) {
+            cb(err, res);
+          });
+        } else {
+          console.log("ERR: USER EXISTS");
+        }
+      });  
     }
   }
+
 }
+
 
